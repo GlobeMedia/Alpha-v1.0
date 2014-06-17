51f194ce1f9bd8d349e18dc9fe3da1d4e71c53c8
mirjamsk <mirjamskarica@gmail.com>
2014-06-18 00:48:59 +0200

    Change nav icon color on hover and on click (smaller screens)

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -32,19 +32,49 @@ function makeMagicLine(){
 	recalibrateMagicLine();   
 }
 
+function changeIconToActive(img){
+	var src = img.attr('src');
+	if (src == './images/navIcons/about.svg')
+		img.attr('src', './images/navIcons/aboutActive.svg');
+	else if (src == './images/navIcons/contact.svg')
+		img.attr('src', './images/navIcons/contactActive.svg');
+	else if (src == './images/navIcons/news.svg')
+		img.attr('src', './images/navIcons/newsActive.svg');
+	else if (src == './images/navIcons/projects.svg')
+		img.attr('src', './images/navIcons/projectsActive.svg');
+}
+
+function changeIconToInactive(img){
+	var src = img.attr('src');
+	if (src == './images/navIcons/aboutActive.svg')
+		img.attr('src', './images/navIcons/about.svg');
+	else if (src == './images/navIcons/contactActive.svg')
+		img.attr('src', './images/navIcons/contact.svg');
+	else if (src == './images/navIcons/newsActive.svg')
+		img.attr('src', './images/navIcons/news.svg');
+	else if (src == './images/navIcons/projectsActive.svg')
+		img.attr('src', './images/navIcons/projects.svg');
+}
 
 $(document).ready( function() {
 	makeMagicLine();	
 	// on mouse hover, move magicLine
    	$("#nav_ul li").find('a').hover( function(){
+   		changeIconToInactive(currentItem.find('img'));
    		hoverItem = $(this);
    		hoverMagicLine();
-   	},  returnMagicLine);
+   		changeIconToActive(hoverItem.find('img'));
+   	},  function(){
+   		returnMagicLine();
+   		changeIconToInactive(hoverItem.find('img'));
+   		changeIconToActive(currentItem.find('img'));
+   	});
    	$("#nav_ul li").click(function() {
-   		console.log($(this))
+   		changeIconToInactive(currentItem.find('img'));
    		currentItem.attr('class', '');
    		$(this).attr('class', 'current_page_item');
    		currentItem = $(this);
+   		changeIconToActive(currentItem.find('img'));
    		recalibrateMagicLine();
    	});
 });
